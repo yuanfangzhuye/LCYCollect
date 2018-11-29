c1007fda0f7693228b3c986cd20f75b69a2d2323
李超 <371551077@qq.com>
2018-11-29 16:43:56 +0800

    add pod files

---
+++ CustomUtils.h
@@ -0,0 +1,19 @@
+//
+//  CustomUtils.h
+//  SensorsAnalyticsSDK
+//
+//  Created by 李超 on 2018/11/29.
+//  Copyright © 2018 Sensors Data Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CustomUtils : NSObject
+
++ (void)setbackgroundView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CustomUtils.m
@@ -0,0 +1,18 @@
+//
+//  CustomUtils.m
+//  SensorsAnalyticsSDK
+//
+//  Created by 李超 on 2018/11/29.
+//  Copyright © 2018 Sensors Data Inc. All rights reserved.
+//
+
+#import "CustomUtils.h"
+
+@implementation CustomUtils
+
++ (void)setbackgroundView
+{
+    
+}
+
+@end
